af57d087b50629bde676625808abc354e4da81a4
Jack Lloyd <jack@randombit.net>
2026-04-04 18:52:35 -0400

    Reject output_bits == 0 in SHAKE_128 and SHAKE_256

--- src/lib/hash/shake/shake.cpp
+++ src/lib/hash/shake/shake.cpp
@@ -14,7 +14,7 @@ namespace Botan {
 
 SHAKE_128::SHAKE_128(size_t output_bits) :
       m_keccak({.capacity_bits = 256, .padding = KeccakPadding::shake()}), m_output_bits(output_bits) {
-   if(output_bits % 8 != 0) {
+   if(output_bits == 0 || output_bits % 8 != 0) {
       throw Invalid_Argument(fmt("SHAKE_128: Invalid output length {}", output_bits));
    }
 }
@@ -43,7 +43,7 @@ void SHAKE_128::final_result(std::span<uint8_t> output) {
 
 SHAKE_256::SHAKE_256(size_t output_bits) :
       m_keccak({.capacity_bits = 512, .padding = KeccakPadding::shake()}), m_output_bits(output_bits) {
-   if(output_bits % 8 != 0) {
+   if(output_bits == 0 || output_bits % 8 != 0) {
       throw Invalid_Argument(fmt("SHAKE_256: Invalid output length {}", output_bits));
    }
 }
